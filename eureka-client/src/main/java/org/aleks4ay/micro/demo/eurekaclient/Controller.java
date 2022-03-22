b8b24cafa6859e7230db833d0b5e9a8f69ec3b1f
Oleksii Sergiienko <aleks4ay@gmail.com>
2022-03-22 11:58:41 +0200

    Added work with LoadBalancer

--- eureka-client/src/main/java/org/aleks4ay/micro/demo/eurekaclient/Controller.java
+++ eureka-client/src/main/java/org/aleks4ay/micro/demo/eurekaclient/Controller.java
@@ -1,5 +1,6 @@
 package org.aleks4ay.micro.demo.eurekaclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class Controller {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/test")
     public String test() {
-        return "test eureka-client";
+        return "test eureka-client" + System.lineSeparator() + "instance Id: " + instanceId;
     }
 }
