b67d92faf4c110b38e091972992f599226d2e6c8
Oleksii Sergiienko <aleks4ay@gmail.com>
2022-03-22 11:29:05 +0200

    Added Alias Api-gateway. (To work you need url=localhost:8082/eclient/main/test or url=localhost:8082/main/test)

--- eureka-client/src/main/java/org/aleks4ay/micro/demo/eurekaclient/Controller.java
+++ eureka-client/src/main/java/org/aleks4ay/micro/demo/eurekaclient/Controller.java
@@ -1,9 +1,11 @@
 package org.aleks4ay.micro.demo.eurekaclient;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class Controller {
 
     @GetMapping("/test")
